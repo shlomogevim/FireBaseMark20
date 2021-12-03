67855f0b4618d7f53991c94daaba09fabe374013
shlomo <shlomo.gevim@gmail.com>
2021-12-03 20:22:22 +0200

    Initial commit

---
+++ app/src/main/java/com/sg/firebasemark20/AddThougtActivity.kt
@@ -0,0 +1,74 @@
+package com.sg.firebasemark20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_add_thougt.*
+
+class AddThougtActivity : AppCompatActivity() {
+
+    var selectedCategory = FUNNY
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_thougt)
+    }
+
+    fun addFunnyClick(view: View) {
+        if (selectedCategory == FUNNY) {
+            addFunnyBtn.isChecked = true
+            return
+        }
+        addSeriosBtn.isChecked = false
+        addCrazyBtn.isChecked = false
+        selectedCategory = FUNNY
+    }
+
+    fun addSeriousClick(view: View) {
+        if (selectedCategory == SERIOUS) {
+            addSeriosBtn.isChecked = true
+            return
+        }
+        addCrazyBtn.isChecked = false
+        addFunnyBtn.isChecked = false
+        selectedCategory = SERIOUS
+
+    }
+
+    fun addCrazyClick(view: View) {
+        if (selectedCategory == CRAZY) {
+            addCrazyBtn.isChecked = true
+            return
+        }
+        addSeriosBtn.isChecked = false
+        addFunnyBtn.isChecked = false
+        selectedCategory = CRAZY
+
+    }
+
+    fun addPostClick(view: View) {
+        //add post to firebase
+
+        val data=HashMap<String,Any>()
+        data.put(CATEGORY,selectedCategory)
+        data.put(NUM_COMMENTS,0)
+        data.put(NUM_LIKES,0)
+        data.put(THOUGHT_TXT,addThoughtTxt.text.toString())
+        data.put(TIMESTAMP,FieldValue.serverTimestamp())
+        data.put(USERNAME,addUserNameTxt.text.toString())
+
+        FirebaseFirestore.getInstance().collection(THOUGHT_REF).add(data)
+            .addOnSuccessListener {
+                finish()
+            }
+            .addOnFailureListener {
+                Log.e(TAG,"could not add post exception because --> ${it.message}")
+            }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/sg/firebasemark20/Costant.kt
@@ -0,0 +1,36 @@
+package com.sg.firebasemark20
+
+
+
+
+
+
+
+
+
+const val TAG="aaa"
+
+// Frebase collection reference
+
+const val THOUGHT_REF="thoughts"
+const val USERS_REF="users"
+const val COMMENTS_REF="comments"
+
+//category
+
+const val FUNNY="funny"
+const val CRAZY="crazy"
+const val SERIOUS="serious"
+const val POPULAR="popular"
+
+const val CATEGORY="category"
+const val NUM_COMMENTS="numComments"
+const val NUM_LIKES="numLikes"
+const val THOUGHT_TXT="thoughtTxt"
+const val TIMESTAMP="timestamp"
+const val USERNAME="username"
+
+const val DATE_CREATED="dateCreated"
+const val COMMENTS_TXT="commentText"
+
+const val DOCUMENT_KEY="docId"
