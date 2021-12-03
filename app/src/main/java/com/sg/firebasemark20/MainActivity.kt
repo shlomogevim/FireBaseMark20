2a96fa123481e6a348f06dedd7a7da7d40140cda
shlomo <shlomo.gevim@gmail.com>
2021-12-04 01:35:43 +0200

    after thought Adapter

--- app/src/main/java/com/sg/firebasemark20/MainActivity.kt
+++ app/src/main/java/com/sg/firebasemark20/MainActivity.kt
@@ -10,55 +10,68 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sg.firebasemark20.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+
+    lateinit var selectCategory: String
+    lateinit var thoughtsAdapter: ThoughtsAdapter
+    val thoughts= arrayListOf<Thought>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-
-            val intent= Intent(this,AddThougtActivity::class.java)
+        fab.setOnClickListener { view ->
+            val intent = Intent(this, AddThougtActivity::class.java)
             startActivity(intent)
-
-
         }
+        thoughtsAdapter= ThoughtsAdapter((thoughts))
+        thoughtListView.adapter=thoughtsAdapter
+        val layoutManager=LinearLayoutManager(this)
+        thoughtListView.layoutManager=layoutManager
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    fun mainFunnyClicked(view: View) {
+        mainFunnyBtn.isChecked = true
+        mainSeriousBtn.isChecked = false
+        mainCrazyBtn.isChecked = false
+        mainPopularBtn.isChecked = false
+        selectCategory = FUNNY
+        /*thoughtsListener.remove()
+        setListener()*/
+    }
+    fun mainSeriousClicked(view: View) {
+        mainFunnyBtn.isChecked = false
+        mainSeriousBtn.isChecked = true
+        mainCrazyBtn.isChecked = false
+        mainPopularBtn.isChecked = false
+        selectCategory = SERIOUS
+        /*thoughtsListener.remove()
+        setListener()*/
+    }
+    fun mainCreazyClicked(view: View) {
+        mainFunnyBtn.isChecked = false
+        mainSeriousBtn.isChecked = false
+        mainCrazyBtn.isChecked = true
+        mainPopularBtn.isChecked = false
+        selectCategory = CRAZY
+       /* thoughtsListener.remove()
+        setListener()*/
+    }
+    fun mainPopularClicked(view:View) {
+        mainFunnyBtn.isChecked = false
+        mainSeriousBtn.isChecked = false
+        mainCrazyBtn.isChecked = false
+        mainPopularBtn.isChecked = true
+        selectCategory = POPULAR
+       /* thoughtsListener.remove()
+        setListener()*/
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
---
+++ app/src/main/java/com/sg/firebasemark20/ThoughtsAdapter.kt
@@ -0,0 +1,48 @@
+package com.sg.firebasemark20
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class ThoughtsAdapter(val thoughts: ArrayList<Thought>) :
+    RecyclerView.Adapter<ThoughtsAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent?.context).inflate(R.layout.thought_list_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder?.bindThought(thoughts[position])
+    }
+
+    override fun getItemCount() = thoughts.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val username = itemView?.findViewById<TextView>(R.id.listViewUsername)
+        val timestamp = itemView?.findViewById<TextView>(R.id.listViewTimestamp)
+        val thoughtTxt = itemView?.findViewById<TextView>(R.id.listViewToughtTxt)
+        val numLikes = itemView?.findViewById<TextView>(R.id.listViewNumLikes)
+        val likesImage = itemView?.findViewById<ImageView>(R.id.listViewLikesImage)
+
+        fun bindThought(thougt: Thought) {
+            username?.text = thougt.userName
+            thoughtTxt?.text = thougt.thoughtTxt
+            numLikes?.text = thougt.numLikes.toString()
+
+            val dateFormator = SimpleDateFormat("MMM d, h:mm a", Locale.getDefault())
+            val dateString = dateFormator.format(thougt.timestamp)
+            timestamp?.text = dateString
+
+        }
+
+    }
+}
